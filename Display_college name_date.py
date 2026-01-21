066980ab3369f40d0cac786be40c71a6312b1095
MihirSharma0 <154828607+MihirSharma0@users.noreply.github.com>
2026-01-21 11:26:03 +0530

    Update Display_college name_date.py

--- Display_college name_date.py
+++ Display_college name_date.py
@@ -3,4 +3,6 @@ from datetime import date
 today = date.today()
 print("The name of my college is :-",name)
 print("Today's date is :- ",today)
+print("The id of name is :- ",id(name))
+print("The id of today is :- ",id(today))
 
