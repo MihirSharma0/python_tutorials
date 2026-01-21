dcabfead85b16c0f566e6a24ffd61a1c0993802c
MihirSharma0 <154828607+MihirSharma0@users.noreply.github.com>
2026-01-21 11:25:13 +0530

    Update Display_college name_date.py

--- Display_college name_date.py
+++ Display_college name_date.py
@@ -1,3 +1,6 @@
 name = str(input("Enter the name of your college = "))
 from datetime import date
 today = date.today()
+print("The name of my college is :-",name)
+print("Today's date is :- ",today)
+
