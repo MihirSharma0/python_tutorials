61ef855b3d19fe7362057b91283b352e576ad8e3
MihirSharma0 <154828607+MihirSharma0@users.noreply.github.com>
2026-01-23 05:47:04 +0530

    Update pattern1.py

--- pattern1.py
+++ pattern1.py
@@ -1,7 +1,7 @@
-for x in range(5):
-    for y in range(x):
-        print("*",end="")
-    print()  
+# for x in range(5):
+#     for y in range(x):
+#         print("*",end="")
+#     print()  
 
 for x in range(5,0,-1):
     for y in range(x):
