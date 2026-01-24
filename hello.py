a6ba0201ef76aa5ffcbc8de2dfc4fb2f3652d93a
MihirSharma0 <154828607+MihirSharma0@users.noreply.github.com>
2026-01-24 19:16:33 +0530

    Auto-commit #9 - 2026-01-24 19:16:33

--- hello.py
+++ hello.py
@@ -5,3 +5,5 @@ if n1>=n2>=n3:
     print("The sorted list is = ",n3,n2,n1)
 elif n3>=n2>=n1:
     print("The sorted list is = ",n1,n2,n3)
+elif n2>=n1>=n3:
+    print("The sorted list is = ",n3,n1,n2)
