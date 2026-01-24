b06e87b16c7a3fac62a4307a6b93aaaf289bc8f0
MihirSharma0 <154828607+MihirSharma0@users.noreply.github.com>
2026-01-24 19:16:19 +0530

    Auto-commit #7 - 2026-01-24 19:16:19

--- hello.py
+++ hello.py
@@ -2,4 +2,5 @@ n1=int(input("Enter 1st number = "))
 n2=int(input("Enter the 2nd number = "))
 n3=int(input("Enter the 3rd number = ")) 
 if n1>=n2>=n3:
-        print("The sorted list is = ",n3,n2,n1)
+    print("The sorted list is = ",n3,n2,n1)
+elif n3>=n2>=n1:
