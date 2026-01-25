e9d3872a95c5c498035546c39ab5ec6d6c7ced8b
MihirSharma0 <154828607+MihirSharma0@users.noreply.github.com>
2026-01-25 05:53:46 +0530

    Auto-commit #30 - 2026-01-25 05:53:46

--- sqare_cube.py
+++ sqare_cube.py
@@ -1,4 +1,4 @@
 n=int(intput("enter any number = "))
 print("the sqare of the number is =",n*n)
 print("the cube of the number is =",n*n*n)
-print("the program ended succcessfully")
+print("the program ended ")
